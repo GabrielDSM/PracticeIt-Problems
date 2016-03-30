13e0c2edeab26213eede3301718f796612360faf
GabrielDSM <gabriel.menescal@hotmail.com>
2016-03-30 08:38:30 -0700

    Added commenting and slight update to formatting

--- ArrayListProblems.java
+++ ArrayListProblems.java
@@ -1,11 +1,18 @@
+// Gabriel Menescal
+// This file contains various solutions to ArrayList practice-it problems
+
 public class ArrayListProblems {
+   
+   // A method that takes an ArrayList of Strings as a parameter and that returns the length 
+   // of the longest string in the list. E.g. ArrayList [] --> Output: 0 
+   // ArrayList ["or", "not", "to", "be", "hamlet"] --> Output: 6
    public int maxLength(ArrayList<String> list) {
-       int max = 0;
-       for (int i = 0; i < list.size(); i++) {
-          if (list.get(i).length() > max) {
-             max = list.get(i).length();
-          }
-       }
-       return max;
-    }
+      int max = 0;
+      for (int i = 0; i < list.size(); i++) {
+         if (list.get(i).length() > max) {
+            max = list.get(i).length();
+         }
+      }
+      return max;
+   }
 }
