2e4b24a25f0ea51ce3e2857cac4e5b1c3c722da7
GabrielDSM <gabriel.menescal@hotmail.com>
2016-04-01 00:05:44 -0700

    Solved an ArrayList problem

--- ArrayListProblems.java
+++ ArrayListProblems.java
@@ -25,4 +25,32 @@ public class ArrayListProblems {
          }
       }
    }
+   
+   // A method that takes an ArrayList of Strings as a parameter and that replaces every string with two 
+   // of that string. E.g. ["how", "are", "you?"] --> Output: how, how, are, are, you?, you?]
+   public void doubleList(ArrayList<String> list) {
+      for(int i = list.size() - 1; i >= 0; i--) {
+         list.add(i, list.get(i));
+      }
+   }
+   
+   // A method hat takes an ArrayList of integers as a parameter and that moves the minimum value in the list 
+   // to the front, otherwise preserving the order of the elements. E.g. [6, 1, 4, -2] --> Output: [-2, 6, 1, 4]
+   public void minToFront(ArrayList<Integer> list) {
+      int min;
+      if (list.size() > 0) {
+         min = list.get(0);
+         int index = 0;
+         for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < min) {
+               min = list.get(i);
+               index = i;
+            }
+         }
+         for (int i = index; i > 0; i--) {
+            list.set(i,list.get(i - 1));
+         }
+         list.set(0,min);
+      }
+   }
 }
