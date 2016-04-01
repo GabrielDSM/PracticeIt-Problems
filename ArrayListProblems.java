62f52950c74800444f20f14e8f13e50dcd941f1c
GabrielDSM <gabriel.menescal@hotmail.com>
2016-03-31 23:33:44 -0700

    Solved an ArrayList problem

--- ArrayListProblems.java
+++ ArrayListProblems.java
@@ -15,4 +15,14 @@ public class ArrayListProblems {
       }
       return max;
    }
+   
+   // A method that takes an ArrayList of Strings as a parameter and that removes all of the strings 
+   // of even length from the list. E.g. ["even", "odd", "ev", "o"] --> Output: [odd, o]
+   public void removeEvenLength(ArrayList<String> list) {
+      for (int i = list.size() - 1; i >= 0; i--) {
+         if (list.get(i).length() % 2 == 0) {
+            list.remove(i);
+         }
+      }
+   }
 }
