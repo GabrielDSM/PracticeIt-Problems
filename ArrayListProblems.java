f6314e2a6263cc74d5b0e9aaa7e41eb937b8c856
GabrielDSM <gabriel.menescal@hotmail.com>
2016-04-04 00:20:43 -0700

    Solved an ArrayList problem

--- ArrayListProblems.java
+++ ArrayListProblems.java
@@ -81,4 +81,16 @@ public class ArrayListProblems {
          }
       }
    }
+   
+   // A method that accepts four parameters: an ArrayList of integers, an element value, a starting index, and an ending index. 
+   // The method's behavior is to remove all occurrences of the given element that appear in the list between the starting index 
+   // (inclusive) and the ending index (exclusive). Other values and occurrences of the given value that appear outside the given 
+   // index range are not affected. E.g. [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10], 9, 8, 11 --> Output: [1, 2, 3, 4, 5, 6, 7, 8, 10]
+   public void removeInRange(ArrayList<Integer> list, int value, int start, int end) {
+      for (int i = end - 1; i >= start; i--) {
+         if (list.get(i) == value) {
+            list.remove(i);
+         }
+      }
+   }
 }
