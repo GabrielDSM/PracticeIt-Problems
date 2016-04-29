c7cd466b5cf9d467a4f1b2183e514f5afa65d655
Gabriel Menescal <gabriel.menescal@hotmail.com>
2016-04-28 23:59:15 -0700

    Commenting

--- MapProblems.java
+++ MapProblems.java
@@ -2,6 +2,10 @@
 // This file contains various solutions to Map practice-it problems
 
 public class MapProblems {
+   
+   // A method that, takes a Map<String,String>, where the keys are song names mapping
+   // to artists. The method returns a Map<String, Set<String>> where the string (artist names)
+   // map to a Set<String> of all their songs.
    public Map<String, Set<String>> artists (Map<String, String> songs) {
       Map<String, Set<String>> result = new TreeMap<String, Set<String>>();
       for (String s : original.keySet()) {
