d316e459968f1a08a273550e57044ded2c70dd08
Gabriel Menescal <gabriel.menescal@hotmail.com>
2016-04-28 23:27:59 -0700

    Solved a new Set problem

--- SetProblems.java
+++ SetProblems.java
@@ -9,4 +9,22 @@ public class SetProblems {
       }
       return set.size();
    }
+   
+   public int numInCommon(List<Integer> l1, List<Integer> l2) {
+      Set<Integer> s1 = new HashSet<Integer>();
+      Set<Integer> s2 = new HashSet<Integer>();
+      int common = 0;
+      for (int n : l1) {
+         s1.add(n);
+      }
+      for (int n : l2) {
+         s2.add(n);
+      }
+      for (int n : s1) {
+         if (s2.contains(n)) {
+            common++;
+         }
+      }
+      return common;
+   }
 }
